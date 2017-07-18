395ba00bdbf13b0766681e55c7b7d9d5ab7d561a
Phoebe-zhang <3144303497@qq.com>
2017-07-18 15:59:18 +0800

    Add files via upload
    test

---
+++ ZXPPodTest.h
@@ -0,0 +1,16 @@
+//
+//  ZXPPodTest.h
+//  ZPPodTest
+//
+//  Created by mac-mini1 on 2017/7/18.
+//  Copyright © 2017年 sinocev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZXPPodTest : NSObject
+
+- (void)testNSLogWithString:(NSString *)string;
+
+
+@end
---
+++ ZXPPodTest.m
@@ -0,0 +1,18 @@
+//
+//  ZXPPodTest.m
+//  ZPPodTest
+//
+//  Created by mac-mini1 on 2017/7/18.
+//  Copyright © 2017年 sinocev. All rights reserved.
+//
+
+#import "ZXPPodTest.h"
+
+@implementation ZXPPodTest
+
+- (void)testNSLogWithString:(NSString *)string
+{
+    NSLog(@"%@", string);
+}
+
+@end
